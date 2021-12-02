b456a92c2616c89f5d27d2b75412ceee7453794e
Xiaohan Liu <danae0730@gmail.com>
2021-12-02 13:13:38 -0800

    update

--- postgres.sql
+++ postgres.sql
@@ -40,12 +40,17 @@ CREATE TABLE characteristics_review (
   value INT NOT NULL
 );
 
+CREATE TABLE index (
+  product_id INT NOT NULL,
+  review_id INT NOT NULL
+)
+
 -- ETL:
 
-COPY reviewlist from 'review.csv' DELIMITER ',' CSV HEADER;
+COPY reviewlist from 'reviews.csv' DELIMITER ',' CSV HEADER;
 COPY photo from 'reviews_photos.csv' DELIMITER ',' CSV HEADER;
 COPY characteristics from 'characteristics.csv' DELIMITER ',' CSV HEADER;
-COPY characteristics_review from 'characteristics_review.csv' DELIMITER ',' CSV HEADER;
+COPY characteristics_review from 'characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
 
 -- convert data before send back
 update reviewlist
@@ -58,40 +63,32 @@ SELECT b.product_id,a.review_id,c.id as characteristic_id, c.name, a.value from 
 left join reviewlist b
 on a.review_id = b.id
 left join characteristics c
-on a.characteristics_id = c.id and b.product_id = c.product_id
-
--- create table to get the average characteristic score for each product
-
-create table characteristics_total as
-select product_id, name, sum(value) as sum, (sum(value)/count(review_id))::numeric(10,4) as avg_value
-  from characteristics_combine where product_id = 37211
-group by product_id, name;
+on a.characteristics_id = c.id and b.product_id = c.product_id;
 
 
 
 -- add page column to reviewlist so that review can be filtered by page and count
-create table reviewlist_page as
+create table testing as
 select *, (row_number()over(partition by product_id order by helpfulness)-1) / 10+1 as page
 from reviewlist;
-alter table reviewlist_page
+
+alter table testing
 add recommend_transform INT ;
 
 -- add a recommend_transform to make it easier when sending the data back to users
 
-update reviewlist_page
+update testing
 set recommend_transform = 0 where recommend = false;
-update reviewlist_page
+update testing
 set recommend_transform = 1 where recommend = true;
 
 
-alter table reviewlist_page
-add PRIMARY KEY(id);
+create index review_index on testing(review_id);
+create index review_index on photo(review_id);
+create index product_index on characteristics_combine(product_id);
+create index product_index on index(product_id);
 
 
-
--- make table reviewlist_page id column auto_increemnet
--- create sequence seq increment 1 start 5774953;
--- alter  table reviewlist_page alter column id set default nextval('seq');
 
 
 -- alter table characteristics_combine
@@ -103,3 +100,4 @@ add PRIMARY KEY(id);
 --  ADD CONSTRAINT fk FOREIGN KEY (review_id) REFERENCES testing (id);
 
 -- alter table testing add constraint review_id primary key(id);
+
