8d0e57b7f7ca96e3f52698b42abc229f5abff227
Xiaohan Liu <danae0730@gmail.com>
2021-12-02 13:11:31 -0800

    add api end point and query method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const pgp = require("pg-promise")();
+//need to chagne connection when deployed
 const connection = {
   host: "localhost",
   user: "postgres",
@@ -23,43 +24,21 @@ app.listen(3000, () => {
 //photo[{id, url}]]
 
 app.get("/reviews/", (req, res) => {
-  var page = req.query.page !== undefined ? req.query.page : 1;
-  var count = req.query.count !== undefined ? req.query.count : 5;
+  var page = req.query.page || 1;
+  var count = req.query.count || 5;
   var product_id = req.query.product_id;
   var sort = req.query.sort;
+  var sorting = sort !== undefined ? `order by a.${sort} DESC` : "";
   var text = "";
-
-  if (sort === "new") {
-    text = `select a.id as review_id, a.rating, a.summary, a.recommend, a.reponse as response, a.body,a.date,
-    a.reviewer_name,a.helpfulness, json_agg(json_build_object('id',b.id,'url',b.url)) as photos from testing a left join photo b
-    on a.id = b.review_id
-    left join index c
-    on a.id = c.review_id
-    where c.product_id = ${product_id} and a.reported <> 't'
-    group by a.id, a.rating, a.summary,a.recommend,a.reponse,a.body,a.date, a.reviewer_name, a.helpfulness
-     order by a.date DESC
-    limit ${count}`;
-  } else if (sort === "helpfulness") {
-    text = `select a.id as review_id, a.rating, a.summary, a.recommend, a.reponse as response, a.body,a.date,
-    a.reviewer_name,a.helpfulness, json_agg(json_build_object('id',b.id,'url',b.url)) as photos from testing a left join photo b
-    on a.id = b.review_id
-    left join index c
-    on a.id = c.review_id
-    where c.product_id = ${product_id} and a.reported <> 't'
-    group by a.id, a.rating, a.summary,a.recommend,a.reponse,a.body,a.date, a.reviewer_name, a.helpfulness
-     order by a.helpfulness DESC
-     limit ${count}`;
-  } else {
-    text = `select a.id as review_id, a.rating, a.summary, a.recommend, a.reponse as response, a.body,a.date,
-    a.reviewer_name,a.helpfulness, json_agg(json_build_object('id',b.id,'url',b.url)) as photos from testing a left join photo b
-    on a.id = b.review_id
-    left join index c
-    on a.id = c.review_id
-    where c.product_id = ${product_id} and a.reported <> 't'
-    group by a.id, a.rating, a.summary,a.recommend,a.reponse,a.body,a.date, a.reviewer_name, a.helpfulness
-    limit ${count} `;
-  }
-
+  text = `select a.id as review_id, a.rating, a.summary, a.recommend, a.reponse as response, a.body,a.date,
+  a.reviewer_name,a.helpfulness, json_agg(json_build_object('id',b.id,'url',b.url)) as photos from testing a left join photo b
+  on a.id = b.review_id
+  left join index c
+  on a.id = c.review_id
+  where c.product_id = ${product_id} and a.reported <> 't' and page = ${page}
+  group by a.id, a.rating, a.summary,a.recommend,a.reponse,a.body,a.date, a.reviewer_name, a.helpfulness
+  ${sorting}
+  limit ${count}`;
   db.query(text)
     .then((response) => {
       console.log("send the data back");
@@ -80,20 +59,28 @@ app.get("/reviews/meta", (req, res) => {
   var temp = [];
   var result = {};
   result["product_id"] = product_id;
-
   db.query(
-    `select a.rating, count(*) from testing a left join index b on a.id = b.review_id where b.product_id=${product_id} group by a.rating`
+    `select a.rating,a.recommend_transform as recommend, count(*) as count from testing a
+     left join index b on a.id=b.review_id
+     where b.product_id= ${product_id} group by a.rating, a.recommend_transform`
   )
     .then((data) => {
-      temp.push(data);
-      return db.query(
-        `select a.recommend_transform as recommend, count(*) from testing a left join index b on a.id = b.review_id where b.product_id =${product_id} group by a.recommend_transform
-        `
-      );
-    })
-    .then((data) => {
-      console.log(data);
-      temp.push(data);
+      const rate = {};
+      const rem = {};
+      data.forEach((item) => {
+        rate[item.rating] = item.count;
+      });
+      data.forEach((item) => {
+        if (rem[item.recommend]) {
+          rem[item.recommend] = (
+            parseInt(rem[item.recommend]) + parseInt(item.count)
+          ).toString();
+        } else {
+          rem[item.recommend] = item.count;
+        }
+      });
+      result["ratings"] = rate;
+      result["recommend"] = rem;
       return db.query(
         `select product_id, name, (sum(value)/count(review_id))::numeric(10,4) as avg_value
         from characteristics_combine
@@ -103,16 +90,8 @@ app.get("/reviews/meta", (req, res) => {
     })
 
     .then((data) => {
-      temp.push(data);
-      console.log("data", data);
-      const rate = {};
-      const rem = {};
       const char = {};
-      temp[0].forEach((item) => (rate[item.rating] = item.count));
-      result["ratings"] = rate;
-      temp[1].forEach((item) => (rem[item.recommend] = item.count));
-      result["recommended"] = rem;
-      temp[2].forEach((item) => {
+      data.forEach((item) => {
         char[item.name] = { value: item.avg_value };
       });
       result["characteristics"] = char;
